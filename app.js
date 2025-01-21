a65cd396a6cddb18dbc23d822bbd5d286bb42ce5
SamuelDV <dominguezvirelassamuel13@gmail.com>
2025-01-20 18:12:51 -0600

    Se agrego la estructura principal del JS

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+var listaAmigos = [];
+
+
+//Funcion para agregar un amigo a la lista
+function agregarAmigo(){
+    
+}
+
+
+//Funcion para retornar un amigo de forma aleatoria
+function sortearAmigo(){
+
+}
