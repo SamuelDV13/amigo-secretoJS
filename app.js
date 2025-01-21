658b70450d0c233d68e310f4cd99cd6348999ac1
SamuelDV <dominguezvirelassamuel13@gmail.com>
2025-01-20 18:37:35 -0600

    Se implemento la función para agregar a un amigo

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ var listaAmigos = [];
 
 //Funcion para agregar un amigo a la lista
 function agregarAmigo(){
-    
+    listaAmigos.push(document.getElementById("amigo").value);
+    limpiarCampo("amigo");
+    mostrarLista();
 }
 
 
 //Funcion para retornar un amigo de forma aleatoria
 function sortearAmigo(){
 
+}
+
+
+function mostrarLista(){
+        document.getElementById("listaAmigos").innerText += `${listaAmigos[listaAmigos.length - 1]} \n`;
+}
+
+function limpiarCampo(campo){
+    document.getElementById(campo).value = "";
 }
